e42fbfece778a72c515f5b19756a7429f33bab0d
Jake Munz <jake.p.munz@gmail.com>
2018-11-21 11:53:53 -0600

    Updated main.js
    In the following script we select all the toggle labels and listen for keydown events. If the pressed key is either the enter or the spacebar keys, we trigger a click on the label.

--- main.js
+++ main.js
@@ -1,75 +1,12 @@
-//Based on the Scroller function from @sallar
-var $content = $('header .content')
-  , $blur    = $('header .overlay')
-  , wHeight  = $(window).height();
+let myLabels = document.querySelectorAll('.lbl-toggle');
 
-$(window).on('resize', function(){
-  wHeight = $(window).height();
+Array.from(myLabels).forEach(label => {
+  label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
 });
-
-window.requestAnimFrame = (function()
-{
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
-})();
-
-function Scroller()
-{
-  this.latestKnownScrollY = 0;
-  this.ticking            = false;
-}
-
-Scroller.prototype = {
- 
-  init: function() {
-    window.addEventListener('scroll', this.onScroll.bind(this), false);
-    $blur.css('background-image',$('header:first-of-type').css('background-image'));
-  },
-
-
-  onScroll: function() {
-    this.latestKnownScrollY = window.scrollY;
-    this.requestTick();
-  },
-
-  
-  requestTick: function() {
-    if( !this.ticking ) {
-      window.requestAnimFrame(this.update.bind(this));
-    }
-    this.ticking = true;
-  },
-
-  update: function() {
-    var currentScrollY = this.latestKnownScrollY;
-    this.ticking       = false;
-    
-    
-    var slowScroll = currentScrollY / 2
-      , blurScroll = currentScrollY * 2
-      , opaScroll = 1.4 - currentScrollY / 400;
-   if(currentScrollY > wHeight)
-     $('nav').css('position','fixed');
-   else
-     $('nav').css('position','absolute');
-    
-    $content.css({
-      'transform'         : 'translateY(' + slowScroll + 'px)',
-      '-moz-transform'    : 'translateY(' + slowScroll + 'px)',
-      '-webkit-transform' : 'translateY(' + slowScroll + 'px)',
-      'opacity' : opaScroll
-    });
-    
-    $blur.css({
-      'opacity' : blurScroll / wHeight
-    });
-  }
-};
-
-
-var scroller = new Scroller();  
-scroller.init();
